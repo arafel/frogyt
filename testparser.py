9b5277908d67948aa00c61b7fb34bddd4f6b4d58
Paul Walker <paul+gitbox@blacksun.org.uk>
2013-10-01 23:11:46 +0100

    mocked up testparser, works really well - integrate it into main

---
+++ testparser.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse, string
+
+teststrings = [ "delete t1", "delete", "delete -t t1 t2", "list t1", "", "list" ]
+
+def delete_things(args):
+	if args.testing:
+		print "Only testing"
+	else:
+		print "Not testing"
+	print string.join(args.playlists, ", ")
+
+def list_things(args):
+	print "Listing things, no arguments taken"
+	print args
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+parser_delete = subparsers.add_parser("delete")
+parser_delete.add_argument("-t", "--testing", help="If in testing mode, no changes are made to YouTube playlists", action="store_true")
+parser_delete.add_argument("playlists", metavar="playlist", nargs='+', help="playlists to process")
+parser_delete.set_defaults(func=delete_things)
+
+parser_list = subparsers.add_parser("list")
+parser_list.set_defaults(func=list_things)
+
+for ts in teststrings:
+	print "Running parser with '%s'" % ts
+	try:
+		args, unknown = parser.parse_known_args(ts.split())
+		args.func(args)
+	except:
+		pass
+	print
